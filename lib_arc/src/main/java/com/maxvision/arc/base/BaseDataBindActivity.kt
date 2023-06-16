9aca27565769b9a008264aa31f6a99f9bf3fee24
周俊杰 <1282450075@qq.com>
2023-06-16 17:13:49 +0800

    添加基类fragment

--- lib_arc/src/main/java/com/maxvision/arc/base/BaseDataBindActivity.kt
+++ lib_arc/src/main/java/com/maxvision/arc/base/BaseDataBindActivity.kt
@@ -1,5 +1,6 @@
 package com.maxvision.arc.base
 
+import android.util.Log
 import android.view.LayoutInflater
 import androidx.viewbinding.ViewBinding
 import com.maxvision.arc.ext.saveAs
@@ -15,7 +16,10 @@ abstract class BaseDataBindActivity<DB : ViewBinding> : BaseActivity() {
     lateinit var mBinding : DB
 
     override fun setContentLayout() {
+        // javaClass: 获取当前对象的Class对象
+        // javaClass.genericSuperclass: 获取当前类的父类的泛型类型
         val type = javaClass.genericSuperclass
+        // 将获取到的泛型类型转换为ParameterizedType,并从中提取出实际的类型参数
         val vbClass : Class<DB> = type!!.saveAs<ParameterizedType>().actualTypeArguments[0].saveAs()
         val method = vbClass.getDeclaredMethod("inflate",LayoutInflater::class.java)
         mBinding=method.invoke(this,layoutInflater)!!.saveAsUnChecked()
